f0683cabaed564f0131d11a02fa8d36793248828
asag1 <akvilina.galbuogyte@gmail.com>
2020-02-10 18:52:31 +0200

    adding

--- loan_counter/src/main/java/lt/task/loan_counter/LoanCounterApplication.java
+++ loan_counter/src/main/java/lt/task/loan_counter/LoanCounterApplication.java
@@ -1,14 +1,20 @@
 package lt.task.loan_counter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lt.task.loan_counter.post.Count;
+import lt.task.loan_counter.post.Post_parameters;
 
+import java.io.File;
 import java.io.IOException;
 
 public class LoanCounterApplication {
+    static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
-        Count loanResult = new Count();
-        loanResult.loanCount();
+        File file = new File("C:/Users/W10/Desktop/Java/Mano JAVA mokslai/darbai/loan_counter/loan_counter/loan.json");
+        Post_parameters postIsJsonFailo = mapper.readValue(file, Post_parameters.class);
+        System.out.println(postIsJsonFailo.getAge());
+
 
     }
 }
--- loan_counter/src/main/java/lt/task/loan_counter/post/Count.java
+++ loan_counter/src/main/java/lt/task/loan_counter/post/Count.java
@@ -1,7 +1,6 @@
 package lt.task.loan_counter.post;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -12,42 +11,31 @@ public class Count {
 
     private int minMoneyForPerson = 400;
     private int maxPersonAge = 65;
-    @JsonProperty("loan")
-    private List<Post_parameters> posts;
     ObjectMapper mapper = new ObjectMapper();
+    int answer;
 
     public Count() {
     }
 
-    public List<Post_parameters> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post_parameters> posts) {
-        this.posts = posts;
-    }
-
-    public String loanCount() throws IOException {
-        File file = new File("C:/Users/W10/Desktop/Java/Mano JAVA mokslai/darbai/loan_counter/loan_counter/loan.json");
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Count personParametres = mapper.readValue(file, Count.class);
-        this.posts = personParametres.getPosts();
-        for (int i = 0; i < posts.size(); i++){
-            System.out.println(personParametres.getPosts().get(i).getAge());
-        }
-
-        for (int i = 0; i < posts.size(); i++) {
-            if (18 < (int) personParametres.getPosts().get(i).getAge() && (int) personParametres.getPosts().get(i).getAge() < 65) {
-                int freeMoney = (int) personParametres.getPosts().get(i).getIncomes() - (int) personParametres.getPosts().get(i).getDebt() - (int) personParametres.getPosts().get(i).getFamilyMemberNo() * minMoneyForPerson;
-                if (freeMoney > 0) {
-                    int loan = freeMoney * 12 * (maxPersonAge - (int) personParametres.getPosts().get(i).getAge());
-                    if (loan > 500) {
-                        return ("max loan" + loan);
-                    } else return ("cant get loan");
-                }
-                return ("cant get loan");
-            }
-        }
-        return ("cant get loan");
+    public int loanCount() throws IOException {
+//        File file = new File("C:/Users/W10/Desktop/Java/Mano JAVA mokslai/darbai/loan_counter/loan_counter/loan.json");
+//        Post_parameters postIsJsonFailo = mapper.readValue(file, Post_parameters.class);
+//        answer= postIsJsonFailo.getAge();
+//
+////
+//        for (int i = 0; i < posts.size(); i++) {
+//            if (18 < (int) personParametres.getPosts().get(i).getAge() && (int) personParametres.getPosts().get(i).getAge() < 65) {
+//                int freeMoney = (int) personParametres.getPosts().get(i).getIncomes() - (int) personParametres.getPosts().get(i).getDebt() - (int) personParametres.getPosts().get(i).getFamilyMemberNo() * minMoneyForPerson;
+//                if (freeMoney > 0) {
+//                    int loan = freeMoney * 12 * (maxPersonAge - (int) personParametres.getPosts().get(i).getAge());
+//                    if (loan > 500) {
+//                        return ("max loan" + loan);
+//                    } else return ("cant get loan");
+//                }
+//                return ("cant get loan");
+//            }
+//        }
+//        return ("cant get loan");
+        return answer;
     }
 }
